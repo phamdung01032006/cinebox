42ce8588db54a0a1cbdc9c785b0eeab14e94181f
phamdung01032006 <phamdung01032006@gmail.com>
2026-02-23 12:56:18 +0700

    Thêm một số code phần trang index để tìm phim ngẫu nhiên chiếu preview

---
+++ includes/classes/PreviewProvider.php
@@ -0,0 +1,45 @@
+<?php 
+
+class PreviewProvider {
+
+    private $con;
+    private $username;
+
+    public function __construct($con, $username) {
+
+        $this->con = $con;
+        $this->username = $username;
+
+    }
+
+    public function createPreviewVideo($entity) {
+
+        if($entity == null) {
+            $entity = $this->getRandomEntity();
+        }
+
+        $id = $entity->getId();
+        $name = $entity->getName();
+        $preview = $entity->getPreview();
+        $thumbnail = $entity->getThumbnail();
+        
+
+    }
+
+
+    // chọn film ngẫu nhiên để chiếu preview
+    private function getRandomEntity() {
+
+        $query = $this->con->prepare("SELECT * FROM entities ORDER BY RAND() LIMIT 1");
+
+        $query->execute();
+
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+
+        return new Entity($this->con, $row);
+
+    }
+
+}
+
+?>
--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
 
 require_once("includes/config.php");
+require_once("includes/classes/PreviewProvider.php");
+require_once("includes/classes/Entity.php");
 
 if(!isset($_SESSION["userLoggedIn"])) {
     header("Location: register.php");
 }
+
+$userLoggedIn = $_SESSION["userLoggedIn"];
+
+$preview = new PreviewProvider($con, $userLoggedIn);
+
+echo $preview->createPreviewVideo(null);
 
 ?>
