6e9e09b0ea9e3cf3c3ca83dbb5cf032c20031183
phamdung01032006 <phamdung01032006@gmail.com>
2026-02-23 13:02:50 +0700

    Chiếu được video lên rồi nhưng chưa fit với trình duyệt

--- includes/classes/PreviewProvider.php
+++ includes/classes/PreviewProvider.php
@@ -22,8 +22,14 @@ class PreviewProvider {
         $name = $entity->getName();
         $preview = $entity->getPreview();
         $thumbnail = $entity->getThumbnail();
-        
 
+        return "<div class = 'previewContainer'>
+            <img src='$thumbnail' class='previewImage' hidden>
+
+            <video autoplay muted class='previewVideo'>
+                <source src='$preview' type='video/mp4'>
+            </video>
+        </div>";
     }
 
 
