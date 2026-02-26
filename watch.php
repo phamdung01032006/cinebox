9613179090fa5aaf6c56459875df9b6a933854a9
phamdung01032006 <phamdung01032006@gmail.com>
2026-02-26 18:14:18 +0700

    Sửa được lỗi phát fullscreen lúc xem phim

--- watch.php
+++ watch.php
@@ -1,19 +1,25 @@
 <?php
-
 require_once("includes/header.php");
 
 if(!isset($_GET["id"])) {
-    // stop everything after receiving this line of code exit
     ErrorMessage::show("No ID passed into page");
 }
 
 $video = new Video($con, $_GET["id"]);
 $video->incrementView();
 
+$entity = $video->getEntity();
+$seasonProvider = new SeasonProvider($con, $userLoggedIn);
 ?>
 
-<div class="watchContainer">
-    <video id="watchPlayer" playsinline preload="metadata">
-        <source src="<?php echo htmlspecialchars($video->getFilePath()); ?>" type="video/mp4">
-    </video>
+<div class="watchPage">
+    <div class="watchContainer">
+        <video id="watchPlayer" playsinline preload="metadata" controls>
+            <source src="<?php echo htmlspecialchars($video->getFilePath()); ?>" type="video/mp4">
+        </video>
+    </div>
+
+    <div class="watchEpisodes">
+        <?php echo $seasonProvider->create($entity); ?>
+    </div>
 </div>
