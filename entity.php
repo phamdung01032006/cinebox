8b987e21442bd0696b55aa5fad384d5c7f6a2b74
phamdung01032006 <phamdung01032006@gmail.com>
2026-02-28 09:53:43 +0700

    Thêm mở popup player ở entity.php

--- entity.php
+++ entity.php
@@ -22,4 +22,31 @@ echo $categoryContainers->showCategory($entity->getCategoryId(), "You might also
 
 ?>
 
-<?php require_once("footer.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CineBox</title>
+    <link rel="stylesheet" type="text/css" href="assets/style/style.css"/>
+</head>
+<body>
+    <div id="videoPopup" class="videoPopup">
+
+        <div class="videoPopupBackdrop" onclick="closeVideoPopup()"></div>
+        <div class="videoPopupContent">
+            <button class="videoPopupClose" onclick="closeVideoPopup()">
+                <i class="fa-solid fa-xmark"></i>
+            </button>
+            <h3 id="videoPopupTitle"></h3>
+            <!-- <video id="videoPopupPlayer" controls autoplay>
+                <source src="" type="video/mp4">
+            </video> -->
+            <video id="videoPopupPlayer" playsinline></video>
+        </div>
+    </div>
+
+    <?php require_once("footer.php"); ?>
+</body>
+</html>
